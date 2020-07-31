c415a36b4e58ea4be4c181a13320fd4f4b04883a
Chris <c.lawsonnelson@gmail.com>
2020-07-31 12:34:11 -0400

    basic accordion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
-const App = () => {
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end JavaScript library",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You can use React by creating components",
+  },
+];
+
+export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
-
-export default App;
