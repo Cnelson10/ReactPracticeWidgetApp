141be0005f6a4ed67af8133a709b29009800cafb
Chris <c.lawsonnelson@gmail.com>
2020-08-14 16:56:02 -0400

    useEffect cleanup on dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,18 +6,17 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 
   useEffect(() => {
     const onBodyClick = (event) => {
-        if (ref.current.contains(event.target)) {
-            return;
-          }
-          setOpen(false);
-        });if (ref.current.contains(event.target)) {
-            return;
-          }
-          setOpen(false);
-        });
-    } 
-    document.body.addEventListener("click", (event) => {
-      
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
   }, []);
 
   const renderedOptions = options.map((option) => {
